8a56d03e5d31ff0cd0cd7dd05d6c7128aec80b60
CodeNerdHQ <codenerdhq@gmail.com>
2023-01-31 00:26:17 +0000

    added array sample in java

---
+++ basics/Main.java
@@ -0,0 +1,17 @@
+// Main class for Arrays
+public class Main{
+	public static void main(String[] args) {
+        // Method 1
+        int[] numbers_one = new int[5];
+
+        // Add elements in the array
+        numbers_one[0] = 10;
+        numbers_one[1] = 5;
+        numbers_one[2] = 12;
+        numbers_one[3] = 6;
+        numbers_one[4] = 9;
+
+        // Method 2
+        int[] numbers_two = new int[] {10, 5, 12, 6, 9};
+	}
+}

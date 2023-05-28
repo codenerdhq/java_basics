dd68f4a6046b80e49b994bb6275e14be36923504
CodeNerdHQ <codenerdhq@gmail.com>
2023-05-28 17:29:19 +0100

    added commented code with an example of wrong local var init

--- basics/instance_local_vars/Calculator.java
+++ basics/instance_local_vars/Calculator.java
@@ -11,15 +11,27 @@ public class Calculator {
     }
 
     public Calculator() {
-        
+        // Default constructor.
+        // If this is used,
+        // num1 and num2 will be initialized
+        // with 0
     }
 
     public int addition() {
         // Local variable
-        int sum;
+        int sum;  // sum only exist within this method
         sum = this.num1 + this.num2;
         return sum;
     }
     
+    // THIS IS COMMENTED ON PURPOSE
+    // THIS WOULD RETURN AN ERROR 
+    // AS some_number DOES NOT GET 
+    // A DEFAULT VALUE
+    // public int addition() {
+    //     int some_number;
+    //     return some_number;
+    // }
+
 }
 
